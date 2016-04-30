eb461b13545999e86ab79f9c43cf109c95d2ba5b
Felipmorales <felipmorales@hotmail.com>
2016-04-30 14:47:46 +0100

    Buttons + Receipt (pending the order fix)

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser')
 // body-parser for JSON
 app.use(bodyParser.json())
 
+// Receipt variable
+var ordernum = 100;
+
 // Page token
 var token = "EAAK9h7BPLV0BABWIuq5XvsX0K6iZCaxVvYygz7jQ7fd8koYi75zZCev7X8p1rSe5vu1SDHuaKYm18GE6I6nGqVxnj9ivGvKGBOe4FLrsR2UrDjtQkajLAYSZAfKiplciX4Hw4esGJGS4jKnbxtAJVSiOyJsHXcZD";
 
@@ -30,6 +33,78 @@ app.get('/webhook', function (req, res) {
 function sendTextMessage(sender, text) {
   messageData = {
     text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+// sendTextWithImagesMessage function
+function sendImageMessage(sender) {
+  messageData = {
+    "attachment": {
+      "type": "image",
+      "payload": {
+        "url": "https://upload.wikimedia.org/wikipedia/en/thumb/5/56/Real_Madrid_CF.svg/732px-Real_Madrid_CF.svg.png"
+      }
+     }
+   }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+// Function to send button messages
+function sendButtonsMessage(sender) {
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload":{
+        "template_type": "button",
+        "text": "Where would you like to volunteer this summer?",
+        "buttons":[
+        {
+          "type": "web_url",
+          "title": "Egypt",
+          "url": "http://www.egyptwhereitallbegins.com"
+        },
+        {
+          "type": "web_url",
+          "title": "Brazil",
+          "url": "http://impactbrazil.org/"
+        },
+        {
+          "type": "web_url",
+          "title": "India",
+          "url": "http://discover.aiesec.in/"
+        }
+      ]
+      }
+    }
   }
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -98,7 +173,60 @@ function sendGenericMessage(sender) {
   });
 }
 
-// main shit
+// sendReceiptMessage function
+function sendReceiptMessage(sender) {
+  ordernum = ordernum + 1;
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "receipt",
+        "recipient_name": "Felipe Morales",
+        "order_number": ordernum,
+        "currency": "GBP",
+        "payment_method": "Visa 4447",
+        "elements": {
+          "title": "Global Volunteer - Brazil",
+          "subtitle": "X4Change - Fortaleza",
+          "quantity": 1,
+          "price": 350,
+          "currency": "GBP",
+          "image_url": "http://www.getmetravelled.com/wp-content/uploads/2015/07/best-places-of-fortaleza.jpg",
+          },
+         "address":{
+           "street_1": "17 Woolridge way",
+           "street_2": "Loddiges road",
+           "city": "London",
+           "state": "London",
+           "postal_code": "E9 6PP",
+           "country": "UK"
+         },
+         "summary":{
+           "subtotal":350,
+           "total_tax":0,
+           "total_cost":350
+         }
+         }
+        }
+  };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+// Posting function
 app.post('/webhook', function (req, res) {
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
@@ -112,13 +240,25 @@ app.post('/webhook', function (req, res) {
         sendGenericMessage(sender);
         continue;
       }
+     if (text === 'Receipt') {
+        console.log(ordernum);
+        sendReceiptMessage(sender);
+        continue;
+      }
+     if (text === 'Images') {
+         sendImageMessage(sender);
+         continue;
+      }
+     if (text === 'Options') {
+         sendButtonsMessage(sender);
+         continue;
+     }
       if (event.postback) {
         text = JSON.stringify(event.postback);
         sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
         continue;
       }
-
-      sendTextMessage(sender, "I'm a parrot! ECHO!: "+ text.substring(0, 200));
+      sendTextMessage(sender,"I'm a parrot! ECHO!: "+ text.substring(0, 200));
     }
   }
   res.sendStatus(200);
